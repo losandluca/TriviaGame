6fbe11c68095edd164b2f36b409f60a573536c09
losandluca <lewisalcazar88@gmail.com>
2018-12-08 19:45:12 -0700

    trivia game in progress

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+// variables
+$(document).ready(function(){
+    var count = 0;
+    var time = 31;
+    var isSelected = false;
+    var ticker;
+    var correct = 0;
+    var incorrect = 0;
+    var unanswered = 0;
+
+// arrays for  questions and answers
+    var question = ["What is the closest planet to the Sun?","What is the name of the 2nd biggest planet in our solar system?","What is the hottest planet in our solar system?"
+,"What planet is famous for its big red spot on it?","What planet is famous for the beautiful rings that surround it?","Can humans breathe normally in space as they can on Earth?"
+,"Is the sun a star or a planet?","Who was the first person to walk on the moon?","What planet is known as the red planet?","What is the name of the force holding us to the Earth?"];
+    var answer = [];
+    var firstChoice = [];
+    var secondChoice = [];
+    var thirdChoice = [];
+    var fourthChoice = [];
+
+
+  });
